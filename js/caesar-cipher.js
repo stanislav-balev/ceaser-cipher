c0a0e1cc1f82dd28f9cac99d3ded9706f4a5baa3
stanislav-balev <34137643+stanislav-balev@users.noreply.github.com>
2018-07-08 15:25:01 +0300

    FinalVersion

---
+++ js/caesar-cipher.js
@@ -0,0 +1,38 @@
+/* 
+ * Caesar cipher
+ * 
+ * Copyright (c) 2017 Project Nayuki
+ * All rights reserved. Contact Nayuki for licensing.
+ * https://www.nayuki.io/page/caesar-cipher-javascript
+ */
+
+"use strict";
+
+
+function doCrypt(isDecrypt) {
+	var shiftText = document.getElementById("shift").value;
+	if (!/^-?\d+$/.test(shiftText)) {
+		alert("Shift is not an integer");
+		return;
+	}
+	var shift = parseInt(shiftText, 10);
+	if (shift < 0 || shift >= 26) {
+		alert("Shift is out of range");
+		return;
+	}
+	if (isDecrypt)
+		shift = (26 - shift) % 26;
+	var textElem = document.getElementById("text");
+	textElem.value = caesarShift(textElem.value, shift);
+}
+
+function caesarShift(text, shift) {
+	var result = "";
+	for (var i = 0; i < text.length; i++) {
+		var c = text.charCodeAt(i);
+		if      (65 <= c && c <=  90) result += String.fromCharCode((c - 65 + shift) % 26 + 65);  // Uppercase
+		else if (97 <= c && c <= 122) result += String.fromCharCode((c - 97 + shift) % 26 + 97);  // Lowercase
+		else                          result += text.charAt(i);  // Copy
+	}
+	return result;
+}
